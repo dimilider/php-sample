ea97b14a7a787c2d4dcfb544309dec3bb7386eb2
dimilider <dimitri.lider@outlook.com>
2022-07-28 12:22:52 +0200

    add time

--- index.php
+++ index.php
@@ -5,4 +5,20 @@
  <body>
  <?php echo '<p>Hallo Welt - ich bin eine PHP Anwendung hosted in Azure App Service</p>'; ?>
  </body>
+
+ <body>
+<p id="time"></p>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script type="text/javascript">
+var timestamp = '<?=time();?>';
+function updateTime(){
+  $('#time').html(Date(timestamp));
+  timestamp++;
+}
+$(function(){
+  setInterval(updateTime, 1000);
+});
+</script>
+
 </html>
