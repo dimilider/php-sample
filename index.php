f378c44dd077596e74a0995081dea93c9c83b3f8
dimilider <dimitri.lider@outlook.com>
2022-07-28 12:16:34 +0200

    update

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP-Test</title>
  </head>
  <body>
- <?php echo '<p>Hallo Welt</p>'; ?>
+ <?php echo '<p>Hallo Welt - ich bin eine PHP Anwendung hosted in Azure App Service</p>'; ?>
  </body>
 </html>
